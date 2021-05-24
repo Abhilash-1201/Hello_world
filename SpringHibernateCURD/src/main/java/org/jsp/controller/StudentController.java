c68159b095bca5a0ffe337e225631dacc9005536
Abhilash-1201 <83076098+Abhilash-1201@users.noreply.github.com>
2021-05-24 19:26:48 +0530

    Update StudentController.java

--- SpringHibernateCURD/src/main/java/org/jsp/controller/StudentController.java
+++ SpringHibernateCURD/src/main/java/org/jsp/controller/StudentController.java
@@ -54,7 +54,7 @@ public class StudentController
 			Student searchStudent = studentservice.getStudentById(student.getStudentId());
 			studentResult = searchStudent!= null ? searchStudent : new Student();
 			break;
-			default:
+			default: Out.println("my message");
 		
 		}
 		map.put(STUDENT, studentResult);
